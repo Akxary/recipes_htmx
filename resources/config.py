5342ab0d56c89692b329ed4c83b7ae3fb6a03dd3
Akxary <pollxx97@gmail.com>
2025-12-13 20:19:25 +0300

    Путь до бд изменен на абсолютный. создана первая миграция

--- resources/config.py
+++ resources/config.py
@@ -7,9 +7,9 @@ BASE_DIR = [
     for subpath in path.iterdir()
     if subpath.name == "recipes_htmx"
 ][0]
-DB_PATH = "db_file.db"
+DB_PATH = BASE_DIR/"resources"/"db_file.db"
 A_DB_URL = f"sqlite+aiosqlite:////{DB_PATH}"
-DB_URL = f"sqlite:////{DB_PATH}"
+DB_URL = f"sqlite:///{DB_PATH.as_posix()}"
 
 if __name__ == "__main__":
     print(f"{DB_URL=}")
